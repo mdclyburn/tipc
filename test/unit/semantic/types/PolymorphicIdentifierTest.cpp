4fdf8ad80f75be0c02fb74cae17f60f40f215665
Quintin DeGroot <degrootquintin@gmail.com>
2021-03-31 15:16:59 -0400

    add unit test for polymorphicidentifiervisitor

---
+++ test/unit/semantic/types/PolymorphicIdentifierTest.cpp
@@ -0,0 +1,28 @@
+#include "PolymorphicIdentifierVisitor.h"
+
+#include "ASTHelper.h"
+#include "SymbolTable.h"
+#include "catch.hpp"
+#include <iostream>
+#include <sstream>
+
+TEST_CASE("PolymorphicIdentifierVisitor: poly function id", "[PolymorphicIdentifierVisitor]") {
+	std::stringstream program;
+    program << R"(poly(a, b) {
+ return a(b);
+}
+
+nonpoly(a) {
+ return a+1;
+}
+    )";
+
+    auto ast = ASTHelper::build_ast(program);
+    auto symbols = SymbolTable::build(ast.get());
+
+    PolymorphicIdentifierVisitor visitor(symbols.get());
+    ast->accept(&visitor);
+    
+    REQUIRE(visitor.polymorphicFunctions().count("poly") == 1);
+    REQUIRE(visitor.polymorphicFunctions().count("nonpoly") == 0);
+}
